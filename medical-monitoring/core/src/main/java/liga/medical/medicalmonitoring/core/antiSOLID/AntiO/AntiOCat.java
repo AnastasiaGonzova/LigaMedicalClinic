ec92e8ada95ea0f9f17e545b42d1d8e932fd3b0d
AnastasiaGonzova <gnzv.ololo@gmail.com>
2022-10-20 12:51:23 +0300

    Make AntiO example
    Add classes AntiODog and SoundManager. Rename class AntiO to AntiOCat

---
+++ medical-monitoring/core/src/main/java/liga/medical/medicalmonitoring/core/antiSOLID/AntiO/AntiOCat.java
@@ -0,0 +1,12 @@
+package liga.medical.medicalmonitoring.core.antiSOLID.AntiO;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AntiOCat {
+
+    @NonNull
+    private String name;
+
+}
